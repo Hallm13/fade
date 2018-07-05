fbf6f140873d9ede63746a377d38a038721ea908
hallm13 <hallm0013@gmail.com>
2018-07-05 16:28:46 -0500

    add window.onload function

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,21 @@
-var modal = document.getElementById('createModal');
+window.onload = function() {
+  var modal = document.getElementById('createModal');
 
-var btn = document.getElementById("createBtn");
+  var btn = document.getElementById("createBtn");
 
-var span = document.getElementsByClassName("close")[0];
+  var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
+  btn.onclick = function() {
+    modal.style.display = "block";
 }
 
-span.onclick = function() {
-  modal.style.display = "none";
+  span.onclick = function() {
+    modal.style.display = "none";
 }
 
-window.onclick = function(event) {
-  if (event.target == modal) {
+  window.onclick = function(event) {
+    if (event.target == modal) {
     modal.style.display = "none";
   }
+}
 };
